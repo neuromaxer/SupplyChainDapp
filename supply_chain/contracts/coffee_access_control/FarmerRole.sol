0c717b5038b63f0dc558679ab03d44597bc93516
neuromaxer <neuromaxed@gmail.com>
2022-10-26 07:44:34 +0200

    minor refactor

--- supply_chain/contracts/coffee_access_control/FarmerRole.sol
+++ supply_chain/contracts/coffee_access_control/FarmerRole.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.24;
+pragma solidity >=0.7.0 <0.9.0;
 
 // Import the library 'Roles'
 import "./Roles.sol";
--- supply_chain/contracts/coffee_access_control/Roles.sol
+++ supply_chain/contracts/coffee_access_control/Roles.sol
@@ -1,10 +1,12 @@
-pragma solidity ^0.4.24;
+pragma solidity >=0.7.0 <0.9.0;
 
 /**
  * @title Roles
  * @dev Library for managing addresses assigned to a Role.
  */
 library Roles {
+  // this is a library operaing on Role data type 
+  // Ex: using Roles for Role => myRole.add(account)
   struct Role {
     mapping (address => bool) bearer;
   }
@@ -12,6 +14,8 @@ library Roles {
   /**
    * @dev give an account access to this role
    */
+   // Note that the Role is held in storage which is done so modifying it would persist bw calls
+   // However, how does it reconcile with Library being Stateless? (can't define/modify state variables)
   function add(Role storage role, address account) internal {
     require(account != address(0));
     require(!has(role, account));
